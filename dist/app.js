f49aa0488dc05517908089742768849385ee38b9
Sonam Sherpa <sherpa.sjs@gmail.com>
2023-11-27 14:16:59 -0800

    added eventlistener

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,20 @@
 "use strict";
+// autobind decorator
+// function Autobind(
+//   target: any,
+//   methodName: string,
+//   descriptor: PropertyDescriptor
+// ) {
+//   const originalMethod = descriptor.value;
+//   const adjDescriptor: PropertyDescriptor = {
+//     configurable: true,
+//     get() {
+//       const boundFn = originalMethod.bind(this);
+//       return boundFn;
+//     },
+//   };
+//   return adjDescriptor;
+// }
 // ProjectInput Class
 class ProjectInput {
     constructor() {
@@ -6,8 +22,19 @@ class ProjectInput {
         this.hostElement = document.getElementById("app");
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
-        this.element.id = 'user-input';
+        this.element.id = "user-input";
+        this.titleInputElement = this.element.querySelector('#title');
+        this.descriptionInputElement = this.element.querySelector('#description');
+        this.peopleInputElement = this.element.querySelector('#people');
+        this.configure();
         this.attach();
+    }
+    submitHandler(event) {
+        event.preventDefault();
+        console.log(this.titleInputElement.value);
+    }
+    configure() {
+        this.element.addEventListener("submit", this.submitHandler.bind(this));
     }
     attach() {
         this.hostElement.insertAdjacentElement("afterbegin", this.element);
--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,27 @@
+// autobind decorator
+// function Autobind(
+//   target: any,
+//   methodName: string,
+//   descriptor: PropertyDescriptor
+// ) {
+//   const originalMethod = descriptor.value;
+//   const adjDescriptor: PropertyDescriptor = {
+//     configurable: true,
+//     get() {
+//       const boundFn = originalMethod.bind(this);
+//       return boundFn;
+//     },
+//   };
+//   return adjDescriptor;
+// }
 // ProjectInput Class
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById(
@@ -15,8 +34,24 @@ class ProjectInput {
       true
     );
     this.element = importedNode.firstElementChild as HTMLFormElement;
-    this.element.id = 'user-input'
+    this.element.id = "user-input";
+
+    this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
+
+    this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
+
+    this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
+    
+    
+    this.configure();
     this.attach();
+  }
+  private submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
   }
   private attach() {
     this.hostElement.insertAdjacentElement("afterbegin", this.element);
@@ -24,4 +59,3 @@ class ProjectInput {
 }
 
 const project = new ProjectInput();
-
