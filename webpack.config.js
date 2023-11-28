c312bd152487179c17067cb8fb2cc6cf1c94643d
Sonam Sherpa <sherpa.sjs@gmail.com>
2023-11-28 11:10:46 -0800

    production workflow added

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,34 @@
 const path = require("path");
+
 module.exports = {
-    // entry point
+  mode: "development",
+  // entry point
   entry: "./src/app.ts",
-//   output
+  devServer: {
+    static: [
+      {
+        directory: path.join(__dirname),
+      },
+    ],
+  },
+  //   output
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
+    publicPath: '/dist/'
   },
   devtool: 'inline-source-map', //help in debugging, set sourceMap true in tsconfig.json
-//   tell webpack how to deal with the file
+  //   tell webpack how to deal with the file
   module: {
     rules: [
       {
-        test: /\.ts$/,  // any file ends with .ts should be handle by this rule
-        use: "ts-loader",  // tell webpack to deal ts file with this loader
-        exclude: /node_modules/,  // tell webpack not to look at this modules
+        test: /\.ts$/, // any file ends with .ts should be handle by this rule
+        use: "ts-loader", // tell webpack to deal ts file with this loader
+        exclude: /node_modules/, // tell webpack not to look at this modules
       },
     ],
   },
   resolve: {
-    extensions: [".ts", ".js"],  // tell webpack look for both ts and js file
+    extensions: [".ts", ".js"], // tell webpack look for both ts and js file
   },
 };
